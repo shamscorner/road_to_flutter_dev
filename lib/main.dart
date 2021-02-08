76c99598af9fad4df6daac303f6040b7f6ffa0f4
Shamim Hossain <hossains159@gmail.com>
2021-02-08 09:02:52 +0600

    chore: add cart addition and deletion on items

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,18 @@ class ShoppingList extends StatefulWidget {
 }
 
 class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,28 +49,53 @@ class _ShoppingListState extends State<ShoppingList> {
           children: widget.products.map((Product product) {
             return ShoppingListItem(
               product: product,
+              inCart: _shoppingCart.contains(product),
+              onCartChanged: _handleCartChanged,
             );
           }).toList(),
         ));
   }
 }
 
+// a function type definition
+typedef void CartChangedCallback(Product product, bool inCart);
+
 class ShoppingListItem extends StatelessWidget {
   final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
 
-  ShoppingListItem({this.product});
+  ShoppingListItem({this.product, this.inCart, this.onCartChanged});
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) {
+      return null;
+    }
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () {},
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
       leading: CircleAvatar(
-        backgroundColor: Colors.blue[500],
-        child:
-            Text(product.name[0]), // the initial character of the product name
+        backgroundColor: _getColor(context),
+        child: Text(
+          product.name[0], // the initial character of the product name
+        ),
       ),
       title: Text(
         product.name,
+        style: _getTextStyle(context),
       ),
     );
   }
