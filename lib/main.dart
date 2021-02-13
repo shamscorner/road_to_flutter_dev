af5143fbad8fa0e33090672f20ab2ed97ba35d88
Shamim Hossain <hossains159@gmail.com>
2021-02-13 08:44:15 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Grid view with extent',
+      title: 'List view demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Grid view with extent'),
+      home: MyHomePage(title: 'List view demo'),
     );
   }
 }
@@ -30,23 +30,41 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
       ),
       body: Center(
-        child: _buildGrid(),
+        child: _buildList(),
       ),
     );
   }
 
-  Widget _buildGrid() => GridView.extent(
-        maxCrossAxisExtent: 150,
-        padding: const EdgeInsets.all(4),
-        mainAxisSpacing: 4,
-        crossAxisSpacing: 4,
-        children: _buildGridTileList(30),
+  Widget _buildList() => ListView(
+        children: [
+          _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+          _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+          _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+          _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+          _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+              Icons.theaters),
+          _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+          Divider(),
+          _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+          _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+          _tile(
+              'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+          _tile('La Ciccia', '291 30th St', Icons.restaurant),
+        ],
       );
 
-  List<Container> _buildGridTileList(int count) => List.generate(
-        count,
-        (index) => Container(
-          child: Image.asset('assets/images/pic$index.jpg'),
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+        ),
+        subtitle: Text(subtitle),
+        leading: Icon(
+          icon,
+          color: Colors.blue[500],
         ),
       );
 }
