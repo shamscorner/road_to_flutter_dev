00588cdd6e3c72f7081cefa7bed10438e51826d9
Shamim Hossain <hossains159@gmail.com>
2021-02-13 10:51:03 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,28 +29,38 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: Center(
-          child: Stack(
-            alignment: const Alignment(0.6, 0.6),
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pic.jpg'),
-                radius: 100,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black45,
-                ),
-                child: Text(
-                  'Mia B',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+        body: SizedBox(
+          height: 210.0,
+          child: Card(
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text('1625 Main Street',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                  subtitle: Text('My City, CA 99984'),
+                  leading: Icon(
+                    Icons.restaurant_menu,
+                    color: Colors.blue[500],
                   ),
                 ),
-              )
-            ],
+                Divider(),
+                ListTile(
+                  title: Text('(408) 555-1212',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.contact_phone,
+                    color: Colors.blue[500],
+                  ),
+                ),
+                ListTile(
+                  title: Text('costa@example.com'),
+                  leading: Icon(
+                    Icons.contact_mail,
+                    color: Colors.blue[500],
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
