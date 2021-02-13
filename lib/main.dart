58aa0f10aab32dbfa173b9e3859fd43047925996
Shamim Hossain <hossains159@gmail.com>
2021-02-13 10:38:57 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Stack Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Stack Demo'),
     );
   }
 }
@@ -26,14 +26,32 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Hello World'),
+        appBar: AppBar(
+          title: Text(title),
         ),
-      ),
-    );
+        body: Center(
+          child: Stack(
+            alignment: const Alignment(0.6, 0.6),
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/images/pic.jpg'),
+                radius: 100,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black45,
+                ),
+                child: Text(
+                  'Mia B',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
