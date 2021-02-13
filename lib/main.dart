181edf3344f322913eea8babe9c38a22e3059f42
Shamim Hossain <hossains159@gmail.com>
2021-02-13 11:24:08 +0600

    chore: add title, buttons, and text sections

--- lib/main.dart
+++ lib/main.dart
@@ -18,43 +18,91 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32.0),
-  child: Row(
-    children: [
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      Text('41'), 
-    ],
-  ),
-);
-
 class MyHomePage extends StatelessWidget {
   MyHomePage({this.title});
 
   final String title;
+
+  final Widget _titleSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        Text('41'),
+      ],
+    ),
+  );
+
+  static Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  final Widget _textSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+    ),
+  );
+
+  final Widget _buttonSection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(Colors.blue[500], Icons.call, 'CALL'),
+        _buildButtonColumn(Colors.blue[500], Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(Colors.blue[500], Icons.share, 'SHARE'),
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -63,7 +111,11 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
       ),
       body: Column(
-        children: [titleSection],
+        children: [
+          _titleSection,
+          _buttonSection,
+          _textSection,
+        ],
       ),
     );
   }
